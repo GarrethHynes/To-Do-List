1bc71f226448a2b7cfcfbe49c86e38bb657c64d2
Garrethhynes <garreth.hynes36@gmail.com>
2022-06-06 21:27:38 +0100

    Final

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,36 +3,34 @@ const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 
 const protect = asyncHandler(async (req, res, next) => {
-    let token
+  let token
 
-    // checking for the auth header + making sure its a bearer token
-    if (
-        req.headers.authorization &&
-        req.headers.authorization.startsWith('Bearer')
-    ) {
-        try {
-            // get token from header (we're assigning the token to this variable)
-            token = req.headers.authorization.split(' ')[1]
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(' ')[1]
 
-            // verify token
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-            // get user from the token
-            req.user = await User.findById(decoded.id).select('-password')
+      // Get user from the token
+      req.user = await User.findById(decoded.id).select('-password')
 
-            next()
-
-        } catch (error) {
-            console.log(error)
-            res.status(401)
-            throw new Error('Not authorized')
-        }
+      next()
+    } catch (error) {
+      console.log(error)
+      res.status(401)
+      throw new Error('Not authorized')
     }
-    // if there's no token at all - res.status
-    if (!token) {
-        res.status(401)
-        throw new Error('Not authorized, no token')
-    }
+  }
+
+  if (!token) {
+    res.status(401)
+    throw new Error('Not authorized, no token')
+  }
 })
 
 module.exports = { protect }
